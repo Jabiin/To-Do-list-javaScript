cc7b34d323190d3d6c0b08241f26ce97fc2ce96e
abdurahmanjabiin <jabiin626@gmail.com>
2023-02-12 16:29:04 +0300

    design change

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const addTask = document.getElementById('add-task');
 const taskContainer = document.getElementById('task-container');
 const inputTask = document.getElementById('input-task');
 
-// Event Listener for Add Button
+
 
 addTask.addEventListener('click', function(){
 
